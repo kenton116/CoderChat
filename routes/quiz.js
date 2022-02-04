dd7429c37fa37547292d4019f2b38fefbec4a631
Kenton116 <kento.nakamura2009@gmail.com>
2022-02-04 20:33:09 +0900

    スター・報告機能実装

--- routes/quiz.js
+++ routes/quiz.js
@@ -62,6 +62,7 @@ router.get('/:quizId',(req, res, next) => {
         quiz: quiz,
         adminGithub: process.env.ADMIN_GITHUB /*|| config.admin.github*/,
         adminGoogle: process.env.ADMIN_GOOGLE /*|| config.admin.google*/,
+        csrfToken: req.csrfToken()
       });
     } else {
       const err = new Error('指定されたクイズは見つかりません');
