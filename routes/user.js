19247bdd94bb1bda2c80c0a432a68046eea519d8
Kenton116 <kento.nakamura2009@gmail.com>
2022-02-05 10:19:38 +0900

    ユーザーページ追加

--- routes/user.js
+++ routes/user.js
@@ -4,28 +4,23 @@ const router = express.Router();
 const authenticationEnsurer = require('./authentication-ensurer');
 const User = require('../models/user');
 const Quiz = require('../models/quiz')
-const dayjs = require('dayjs');
-const utc = require('dayjs/plugin/utc')
-const timezone = require('dayjs/plugin/timezone')
-dayjs.extend(utc)
-dayjs.extend(timezone)
 
 router.get('/:userId',authenticationEnsurer,(req, res, next) => {
   User.findOne({
     where: {
       userId: req.params.userId
-    },
+    }
   }).then((user) => {
     Quiz.findAll({
       whre: {
-        createdBy: req.params.userId
+        createdBy: user.userId
       },
       order: [['star','DESC']]
-    }).then((quiz) => {
+    }).then((quizzes) => {
       if(user) {
         res.render('user', {
           user: user,
-          quiz: quiz,
+          quizzes: quizzes,
         });
       } else {
         const err = new Error('指定されたユーザーは見つかりません');
