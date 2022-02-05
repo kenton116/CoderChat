b5d9852cb6312976c758f49019dc7997077e21c9
Kenton116 <kento.nakamura2009@gmail.com>
2022-02-05 12:54:16 +0900

    バグ修正

--- routes/quiz.js
+++ routes/quiz.js
@@ -99,7 +99,7 @@ router.post('/:quizId', authenticationEnsurer, csrfProtection, (req, res, next) 
   }).then((quiz) => {
     if (quiz && (isMine(req, quiz) || isAdmin(req))) {
       if (parseInt(req.query.edit) === 1) {
-        if (req.body.name.length >= 255) {
+        if (req.body.quizName.length >= 255) {
           const err = new Error('クイズを作成または編集できませんでした。もう一度やりなおしてください。');
           next(err);
         }
@@ -108,7 +108,8 @@ router.post('/:quizId', authenticationEnsurer, csrfProtection, (req, res, next) 
         .format('YYYY年MM月DD日 HH時mm分ss秒');
 
         quiz.update({
-          quizName: req.body.name,
+          quizId: quiz.quizId,
+          quizName: req.body.quizName,
           question: req.body.question,
           answer: req.body.answer,
           tag: req.body.tagvalue,
