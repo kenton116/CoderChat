17397cbc8b8bb1d3b78a88bd5f6a9684abf80553
Kenton116 <kento.nakamura2009@gmail.com>
2022-02-05 17:45:42 +0900

    検索機能実装

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,7 +1,7 @@
 'use strict';
 const form = $('#form').get(0);
 const tagValue = $('#tag-value').get(0);
-$('radio').on('click', () => {
+$('button').on('click', () => {
 	const tag = $('input[name="tag"]:checked').val();
 	tagValue.value = tag;
 	console.log(tagValue)
