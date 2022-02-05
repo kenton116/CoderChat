7ccd9dc9877dade4c46244e92380e6ee3f14e046
Kenton116 <kento.nakamura2009@gmail.com>
2022-02-05 12:46:02 +0900

    バグ修正

--- routes/quiz.js
+++ routes/quiz.js
@@ -109,7 +109,7 @@ router.post('/:quizId', authenticationEnsurer, csrfProtection, (req, res, next) 
 
         quiz.update({
           quizId: quiz.quizId,
-          quizName: req.body.quizName,
+          quizName: req.body.quizName,  
           question: req.body.question,
           answer: req.body.answer,
           tag: req.body.tagvalue,
