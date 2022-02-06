048cde17f91d54a2fe86f360aefc3e648f8c1dad
Kenton116 <kento.nakamura2009@gmail.com>
2022-02-06 17:16:45 +0900

    heroku

--- routes/quiz.js
+++ routes/quiz.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 const Quiz = require('../models/quiz');
 const User = require('../models/user');
-const { Op } = require('sequelize')
 const authenticationEnsurer = require('./authentication-ensurer');
 const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc')
@@ -26,9 +25,7 @@ router.get('/new', authenticationEnsurer, csrfProtection, (req, res, next) => {
 router.post('/search/', authenticationEnsurer, (req, res, next) => {
   Quiz.findAll({
     where: {
-      [Op.like]: {
-        tag: req.body.search,
-      }
+      tag: req.body.search,
     },
     order: [['star', 'DESC']]
   }).then(quizzes => {
